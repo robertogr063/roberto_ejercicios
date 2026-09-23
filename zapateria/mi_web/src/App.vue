e8bed6237d6d8e50da9d9d2f585443b3c52dff94
R. Gigante <robertogr063@users.noreply.github.com>
2026-09-23 10:49:48 +0200

    cambiando las iniciales de App.vue

--- zapateria/mi_web/src/App.vue
+++ zapateria/mi_web/src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-import Cabecera from './components/Cabecera.vue'
-import Inicio from './components/Inicio.vue'
-import Modelos from './components/Modelos.vue'
-import Pie from './components/Pie.vue'
+import Cabecera from './components/cabecera.vue'
+import Inicio from './components/inicio.vue'
+import Modelos from './components/modelos.vue'
+import Pie from './components/pie.vue'
 </script>
 
 <template>
